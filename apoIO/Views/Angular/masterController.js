0a3108fdb0e12bd2e2a26f0b09787ebe737b9dba
aasifsayyad <44222398+aasifsayyad@users.noreply.github.com>
2018-10-17 18:49:35 +0530

    171018649

--- apoIO/Views/Angular/masterController.js
+++ apoIO/Views/Angular/masterController.js
@@ -1,4 +1,6 @@
 app.controller('masterController', function($scope) {
+    $scope.itemList = [];
+ 
      var url = baseServiceUrl+'assetdatabases?path=\\\\' + afServerName + '\\' + afDatabaseName; 
             var ajaxEF =  processJsonContent(url, 'GET', null);
                 $.when(ajaxEF).fail(function () {
@@ -13,18 +15,34 @@ app.controller('masterController', function($scope) {
                         console.log("Cannot Find the Element Templates.");
                     });
                     $.when(elementTemplates).done(function () {
-                         var data1 = [];
+                         var list = [];
                         var Items = (elementTemplates.responseJSON.Items);
                         $.each(Items,function(key) {                          
-                           data1.push(Items[key].Name);
-                           console.log("Element Name : "+ data1);
-                           $("#elementTemplates").append('<option>'+Items[key].Name+'</option>');
-                            });                      
+                           list.push(Items[key].Name);  
+                           //list.push('id:'+Items[key].WebId+', name:'+Items[key].Name+',');       
+                           //$("#elementTemplates").append("<option value="+Items[key].WebId+">"+Items[key].Name+"</option>");                          
+                            });   
+                             console.log("Element Name : "+ list);
+                             $scope.list = list;
+
+                                $scope.changedValue = function(item) {
+                                  $scope.itemList.push(item);
+                                }       
                         });
                         //console.log("Event frame created successfully.\n"+JSON.stringify(ajaxEF));
                     });
                     
-                    
+
+    $scope.GetValue = function (fruit) {
+                var fruitId = $scope.ddlFruits;
+                var fruitName = $.grep($scope.list, function (fruit) {
+                    return fruit.Id == fruitId;
+                })[0].Name;
+                alert("Selected Value: " + fruitId + "\nSelected Text: " + fruitName);
+            }
+    
+    /*********chart section**********/
+    
     var dataPoints1 = [];
     var dataPoints2 = [];
       $scope.chart = new CanvasJS.Chart("chartContainer", {
